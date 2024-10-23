d53a04663094e9bbb286023891953b19a9f18b78
Sergio López Martín <sergio.lopez@ciberexperis.es>
2024-10-23 09:19:52 +0200

    Add implementation to test

--- src/test/java/com/demo/services/impl/PersonServiceImplTest.java
+++ src/test/java/com/demo/services/impl/PersonServiceImplTest.java
@@ -6,7 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonServiceImplTest {
 
+
+    @Mock
+    PersonRepository personRepository;
+
+    @InjeckMocks
+    PersonServiceImpl personServiceImpl = new PersonServiceImpl(personRepository);    
+
+
     @Test
     void getByName() {
+
+        String name = "John";
+
+        when(personRepository.findByName(name)).thenReturn(new Person("John", 25));
+
+       
+        List<Person> result =  personServiceImpl.getByName("John");
+        
+        assertEquals(1, result.size());
+        assertEquals("John", result.get(0).getName());
+        
+        verify(personRepository).findByName(name);
+        verifyNoMoreInteractions(personRepository);
+
     }
 }
